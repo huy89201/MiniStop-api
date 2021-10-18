772807002b66d837930c72d353401296c81890b9
Huy Le <huy.le@techupcorp.com>
2021-10-18 10:04:56 +0700

    update headers

--- controlers/verifyToken.js
+++ controlers/verifyToken.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
-    const token = req.header("auth-token");
+    const token = req.header("token");
     if(!token) res.status(401).send("Access Denied");
 
     try {
