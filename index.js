abb9f7bcc24d07cb35539f067690cfbf457a3d3a
Huy Le <huy.le@techupcorp.com>
2021-10-31 14:23:30 +0700

    fix cors bugs

--- index.js
+++ index.js
@@ -18,7 +18,14 @@ mongoose.connect(process.env.DB_CONNECT, () =>
 
 // middlewares
 app.use(express.json());
-app.use(cors());
+
+const corsOptions ={
+  origin:'*', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions));
 
 // router
 const authRoute = require("./routers/auth");
